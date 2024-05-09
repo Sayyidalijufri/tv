7a3a4fe236e10234adcf3bf1f44d33188fbdd9b5
Sayyidalijufri <sayyidalijufri@gmail.com>
2024-05-09 22:03:27 +0700

    fix preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from "@/components/Sidebar";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  preload: false,
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
